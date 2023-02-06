653a4b919479ca975a45ba0be844249b2c70e94f
tomeczek2886 <tomeczek2886@gmail.com>
2023-02-06 16:58:10 +0100

    first commit. Close #1

---
+++ src/main/java/com/example/chucknorrisquotes/service/JokeService.java
@@ -0,0 +1,18 @@
+package com.example.chucknorrisquotes.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeService() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
